8b00a2b33e07bddc1861eb2edcf4c51a446faacc
Thomas Voß <thomas@airmap.com>
2020-01-07 10:05:02 +0100

    Rework tests

--- astrotime_test.go
+++ astrotime_test.go
@@ -5,20 +5,42 @@ import (
 	"math"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 type location struct {
-	timezone  string
+	timezone  *time.Location
 	latitude  float64
 	longitude float64
 }
 
 type times struct {
-	date    string
-	sunrise string
-	sunset  string
-	dawn    string
-	dusk    string
+	midnight time.Time
+	sunrise  time.Time
+	sunset   time.Time
+	dawn     time.Time
+	dusk     time.Time
+}
+
+func mustNewTimes(midnight time.Time, sunrise string, sunset string, dawn string, dusk string) times {
+	f := func(t string) time.Time {
+		ts, err := time.Parse("15:04", t)
+		if err != nil {
+			panic(err)
+		}
+
+		hh, mm, ss := ts.Clock()
+		return midnight.Add(time.Duration(hh) * time.Hour).Add(time.Duration(mm) * time.Minute).Add(time.Duration(ss) * time.Second)
+	}
+
+	return times{
+		midnight: midnight,
+		sunrise:  f(sunrise),
+		sunset:   f(sunset),
+		dawn:     f(dawn),
+		dusk:     f(dusk),
+	}
 }
 
 type test struct {
@@ -29,62 +51,62 @@ type test struct {
 
 // test data from http://www.timeanddate.com/worldclock/sunrise.html
 var (
-	SYDNEY = location{
-		timezone:  "Australia/Sydney",
+	sydney = location{
+		timezone:  mustParseTimezone("Australia/Sydney"),
 		latitude:  -33.900626,
 		longitude: 151.196210,
 	}
-	STOCKHOLM = location{
-		timezone:  "Europe/Stockholm",
+	stockholm = location{
+		timezone:  mustParseTimezone("Europe/Stockholm"),
 		latitude:  59.332953,
 		longitude: 18.059986,
 	}
-	NEWYORK = location{
-		timezone:  "America/New_York",
+	newyork = location{
+		timezone:  mustParseTimezone("America/New_York"),
 		latitude:  40.692197,
 		longitude: -73.940547,
 	}
-	NOVEMBER = "2014-11-01"
-	JULY     = "2015-07-01"
-	MINUTE   = time.Minute
-	DATA     = []*test{
-		&test{times{NOVEMBER, "05:55", "19:23", "05:29", "19:49"}, SYDNEY, MINUTE},
-		&test{times{NOVEMBER, "07:07", "15:55", "06:23", "16:39"}, STOCKHOLM, MINUTE},
-		&test{times{NOVEMBER, "07:26", "17:52", "06:58", "18:21"}, NEWYORK, MINUTE},
-		&test{times{JULY, "07:01", "16:57", "06:33", "17:25"}, SYDNEY, MINUTE},
-		&test{times{JULY, "03:37", "22:06", "02:09", "23:33"}, STOCKHOLM, MINUTE},
-		&test{times{JULY, "05:29", "20:31", "04:55", "21:04"}, NEWYORK, MINUTE},
+
+	minute = time.Minute
+	DATA   = []*test{
+		&test{mustNewTimes(november(sydney.timezone), "05:55", "19:23", "05:29", "19:49"), sydney, minute},
+		&test{mustNewTimes(november(stockholm.timezone), "07:07", "15:55", "06:23", "16:39"), stockholm, minute},
+		&test{mustNewTimes(november(newyork.timezone), "07:26", "17:52", "06:58", "18:21"), newyork, minute},
+		&test{mustNewTimes(july(sydney.timezone), "07:01", "16:57", "06:33", "17:25"), sydney, minute},
+		&test{mustNewTimes(july(stockholm.timezone), "03:37", "22:06", "02:09", "23:33"), stockholm, minute},
+		&test{mustNewTimes(july(newyork.timezone), "05:29", "20:31", "04:55", "21:04"), newyork, minute},
 	}
 )
 
-func TestData(t *testing.T) {
+func july(location *time.Location) time.Time {
+	return time.Date(2014, 7, 1, 0, 0, 0, 0, location)
+}
+
+func november(location *time.Location) time.Time {
+	return time.Date(2014, 11, 1, 0, 0, 0, 0, location)
+}
+
+func mustParseTimezone(tz string) *time.Location {
+	tl, err := time.LoadLocation(tz)
+	if err != nil {
+		panic(err)
+	}
+	return tl
+}
+
+func TestForFixedLocations(t *testing.T) {
 	for _, datum := range DATA {
-		if loc, err := time.LoadLocation(datum.location.timezone); err != nil {
-			t.Fatalf("bad location %s", datum.location.timezone)
-		} else {
-			formatted := fmt.Sprintf("%s 00:00:00", datum.times.date)
-			midnight, err := time.ParseInLocation("2006-01-02 15:04:05", formatted, loc)
-			if err != nil {
-				t.Fatalf("failed to parse date/timestamp '%s': %s", formatted, err)
+		t.Run(fmt.Sprintf("%s-%s", datum.location.timezone, datum.times.midnight), func(t *testing.T) {
+			checkDate := func(t *testing.T, description string, expected time.Time, calculated time.Time) {
+				t.Run(description, func(t *testing.T) {
+					require.GreaterOrEqual(t, datum.error.Seconds(), math.Abs(expected.Sub(calculated).Seconds()))
+				})
 			}
 
-			checkDate := func(description string, timeOfDay string, calculated time.Time) {
-				formatted := fmt.Sprintf("%s %s", datum.times.date, timeOfDay)
-				timestamp, err := time.ParseInLocation("2006-01-02 15:04", formatted, loc)
-				if err != nil {
-					t.Fatalf("failed to parse date/timestamp '%s': %s", formatted, err)
-				}
-				actualError := math.Abs((float64)(timestamp.Sub(calculated)))
-				if actualError > float64(datum.error) {
-					t.Errorf("calculated -> %v, wanted -> %v %f -> (wanted < %d). location=%s date=%s", calculated, timestamp, actualError, datum.error, datum.location.timezone, datum.times.date)
-				}
-			}
-
-			checkDate("dawn", datum.times.dawn, NextDawn(midnight, datum.location.latitude, datum.location.longitude, CIVIL_DAWN))
-			checkDate("sunrise", datum.times.sunrise, NextSunrise(midnight, datum.location.latitude, datum.location.longitude))
-			checkDate("sunset", datum.times.sunset, NextSunset(midnight, datum.location.latitude, datum.location.longitude))
-			checkDate("dusk", datum.times.dusk, NextDusk(midnight, datum.location.latitude, datum.location.longitude, CIVIL_DUSK))
-		}
-
+			checkDate(t, "dawn", datum.times.dawn, NextDawn(datum.times.midnight, datum.location.latitude, datum.location.longitude, CIVIL_DAWN))
+			checkDate(t, "sunrise", datum.times.sunrise, NextSunrise(datum.times.midnight, datum.location.latitude, datum.location.longitude))
+			checkDate(t, "sunset", datum.times.sunset, NextSunset(datum.times.midnight, datum.location.latitude, datum.location.longitude))
+			checkDate(t, "dusk", datum.times.dusk, NextDusk(datum.times.midnight, datum.location.latitude, datum.location.longitude, CIVIL_DUSK))
+		})
 	}
 }
